06caefcbe190cd1641cf547204c6c015c288cfe7
hansokchun <127171688+hansokchun@users.noreply.github.com>
2025-03-17 16:35:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-video_file = 'c:/Users/benet/Desktop/비전/비전1/4620511-uhd_2160_4096_25fps.mp4'
+video_file = 0
 target_format = 'avi'
 target_fourcc = 'XVID'
 
@@ -12,17 +12,17 @@ if video.isOpened():
     fps = video.get(cv.CAP_PROP_FPS)
     wait_msec = int(1 / fps * 1000)
     target = cv.VideoWriter()
-    recording = False 
+    recording = False
 
     while True:
         valid, img = video.read()
         if not valid:
             break
 
-        display = img.copy()
+        display = img.copy() 
 
         if flip:
-            display = cv.flip(display, 1) 
+            display= cv.flip(display, 1) 
 
         if recording:
             center = (50, 50)
@@ -31,14 +31,14 @@ if video.isOpened():
         cv.imshow('Video Player', display)
 
         key = cv.waitKey(wait_msec)
-        if key == 27:  
+        if key == 27: 
             break
         elif key == 32:  
             if recording:
                 target.release()
                 recording = False
             else:
-                target_file = video_file[:video_file.rfind('.')] + '.' + target_format
+                target_file = "recorded_video." + target_format  
                 h, w, *_ = img.shape
                 is_color = (img.ndim > 2) and (img.shape[2] > 1)
                 target.open(target_file, cv.VideoWriter_fourcc(*target_fourcc), fps, (w, h), is_color)
@@ -47,9 +47,10 @@ if video.isOpened():
             flip = not flip
 
         if recording:
-            target.write(display)
+            target.write(display) 
 
     if recording:
         target.release()
 
+    video.release()
     cv.destroyAllWindows()
